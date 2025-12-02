319e0328914e83190065dd365460ebdf86d0334c
alexl <alexlchong@gmail.com>
2025-12-02 18:23:27 -0500

    Set up files

--- src/about.js
+++
@@ -1,8 +0,0 @@
-const aboutUsInfo = document.createElement("p");
-aboutUsInfo.innerText = "We are a lemonade stand restaurant founded by the national Lemonade Association."
-
-function loadAbout(content) {
-    content.appendChild(aboutUsInfo);
-}
-
-export { loadAbout };
