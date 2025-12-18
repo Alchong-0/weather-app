2aa8d8844e55a18b6d96314e8d026b52ccb9b82a
alexl <alexlchong@gmail.com>
2025-12-17 23:30:39 -0500

    Add api functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 // src/index.js
 import "./styles.css";
+import { APIKEY } from "../apikey";
 
-function locationToAPI(location) {
-    return;
+const location = document.getElementById("location");
+const submitFormBtn = document.getElementById("submitForm");
+
+submitFormBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    callAsync(location.value);
+});
+
+async function callAsync(location) {
+    console.log(await locationToAPI(location));
+    // Display information in html
+}
+
+async function locationToAPI(location) {
+    let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+    url += location + "?key=" + APIKEY;
+    const response = await fetch(url);
+    const JSONdata = await response.json();
+    return processJSON(JSONdata);
 }
 
 function processJSON(JSONdata) {
-    return;
+    let weatherInfo = {
+        conditions: JSONdata.currentConditions.conditions,
+        feelslike: JSONdata.currentConditions.feelslike,
+        precip: JSONdata.currentConditions.precip,
+        temp: JSONdata.currentConditions.temp,
+        latitude: JSONdata.latitude,
+        longitude: JSONdata.longitude
+    }
+    return weatherInfo;
 }
